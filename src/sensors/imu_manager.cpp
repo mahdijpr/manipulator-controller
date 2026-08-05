728b3ec879aac97bb651b894c2bae4c51d033611
mahdijpr <153118748+mahdijpr@users.noreply.github.com>
2026-08-05 23:20:30 +0330

    Fuse calibrated IMU angles

--- src/sensors/imu_manager.cpp
+++ src/sensors/imu_manager.cpp
@@ -1,32 +1,54 @@
 #include "imu_manager.h"
 
+#include "common/config.h"
+
+IMUManager::IMUManager()
+    : attitudeFilter_(IMU_COMPLEMENTARY_FILTER_TIME_CONSTANT_S)
+{
+}
+
 bool IMUManager::begin()
 {
-    if(!driver_.begin())
+    if (!driver_.begin())
         return false;
 
-
     calibration_.begin();
-
+    attitudeFilter_.reset();
 
     return true;
 }
 
 bool IMUManager::update()
 {
-
-    if(!driver_.update())
+    if (!driver_.update())
         return false;
 
+    calibration_.update(driver_.getData());
+    data_ = calibration_.getData();
 
-    calibration_.update(
-        driver_.getData()
-    );
+    // Startup calibration establishes the accelerometer-angle and gyro-rate
+    // zero references. Begin fusion from the first calibrated angle so the
+    // filter cannot integrate uncalibrated gyro bias during startup.
+    if (data_.calibrated)
+    {
+        if (!attitudeFilter_.isInitialized())
+        {
+            attitudeFilter_.initialize(data_.rollDeg, data_.pitchDeg);
+        }
+        else
+        {
+            attitudeFilter_.update(
+                data_.rollDeg,
+                data_.pitchDeg,
+                data_.gyroXDegS,
+                data_.gyroYDegS,
+                data_.dtSeconds
+            );
+        }
 
-
-    data_ =
-        calibration_.getData();
-
+        data_.rollDeg = attitudeFilter_.getRollDeg();
+        data_.pitchDeg = attitudeFilter_.getPitchDeg();
+    }
 
     return true;
 }
